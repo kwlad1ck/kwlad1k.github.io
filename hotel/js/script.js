7cebd0d79c52c3be3a397322d6d3825c7b3b3bc8
kwlad1k <67453072+kwlad1k@users.noreply.github.com>
2020-07-01 17:42:18 +0500

    UPD

---
+++ hotel/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.img-slider').slick({
+  	arrows: true,
+	  dots: true,
+	  easing: 'ease',
+	  autoplay: true,
+	  autoplaySpeed: 5000,
+	  draggable:false,
+	  waitForAnimate: true,
+
+  });
+});
