791f2ea600c3ebc7c5b125395b26469ebd954662
kwlad1k <67453072+kwlad1k@users.noreply.github.com>
2020-07-02 23:17:03 +0500

    UPD

--- hotel/js/script.js
+++ hotel/js/script.js
@@ -1,12 +1,42 @@
 $(document).ready(function(){
   $('.img-slider').slick({
   	arrows: true,
-	  dots: true,
+	  dots: false,
 	  easing: 'ease',
 	  autoplay: true,
 	  autoplaySpeed: 5000,
-	  draggable:false,
+	  draggable:true,
 	  waitForAnimate: true,
+	  prevArrow: '<button class="prev arrow-slider"></button>',
+   nextArrow: '<button class="next arrow-slider"></button>',
+  });
+});
 
+$(document).ready(function(){
+  $('.features-slider').slick({
+  arrows: true,
+	 infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  prevArrow: '<button class="prev arrow-slider"></button>',
+  nextArrow: '<button class="next arrow-slider"></button>',
+    responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 });
